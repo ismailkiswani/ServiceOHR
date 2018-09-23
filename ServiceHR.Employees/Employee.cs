aa342f6d32ddd217684f5986db7b317c288ed789
Ismail Al-Kiswani <ismail_kiswani@hotmail.com>
2018-09-23 20:38:10 +0300

    add

--- ServiceHR.Employees/Employee.cs
+++ ServiceHR.Employees/Employee.cs
@@ -12,6 +12,17 @@ namespace ServiceHR.Employees
         private string _emp_mobile;
         private string _emp_jod;
 
+        public Employee(string emp_id, string emp_name, string emp_middlename, string emp_last_name, DateTime emp_dob, string emp_mobile, string emp_jod)
+        {
+            _emp_id = emp_id;
+            _emp_name = emp_name;
+            _emp_middlename = emp_middlename;
+            _emp_last_name = emp_last_name;
+            _emp_dob = emp_dob;
+            _emp_mobile = emp_mobile;
+            _emp_jod = emp_jod;
+        }
+
         public string Emp_id { get => _emp_id; set => _emp_id = value; }
         public string Emp_name { get => _emp_name; set => _emp_name = value; }
         public string Emp_middlename { get => _emp_middlename; set => _emp_middlename = value; }
