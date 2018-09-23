433ac48d27e7abbb76ff1099c0b2c8c88e4f19f0
Ismail Al-Kiswani <ismail_kiswani@hotmail.com>
2018-09-23 12:13:23 +0300

    Adding New Employees Project5

---
+++ ServiceHR.Employees/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceHR.Employees
+{
+    public class Employee
+    {
+        private string _emp_id;
+        private string _emp_name;
+        private string _emp_middlename;
+    }
+}
